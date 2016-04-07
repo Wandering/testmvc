a8475f1452f488279f9620b9a0a1951a3935d611
yyp <wuhun0301@qq.com>
2016-04-07 17:05:02 +0800

    初始化

---
+++ car-war/src/main/java/cn/bwl/controller/JavaInfo.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: gaokao360
+ * $Id:  RoleUserController.java 2015-11-30 22:45:18 $
+ */
+
+package cn.bwl.controller;
+
+import cn.bwl.domain.TestDomain;
+import cn.bwl.service.ITestServcie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value="/info")
+public class JavaInfo {
+
+
+    /**
+     * newInfo
+     * @return
+     */
+    @RequestMapping(value="/newinfo")
+    @ResponseBody
+    public Object newinfo(@RequestParam Integer id){
+        Map<String, Object> map = new HashMap<>();
+        if(id==0) {
+            map.put("imgUrl", "images/n3.jpg");
+            map.put("title", "标题");
+            map.put("content", "内容");
+            map.put("subContent", "内容简介");
+        }
+        return map;
+    }
+
+    /**
+     * photos
+     * @return
+     */
+    @RequestMapping(value="/photos")
+    @ResponseBody
+    public Object photos(){
+        List<Map<String,Object>> list= new ArrayList<>();
+        for(int i = 0 ; i<8;i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("imgUrl", "images/"+(i+4)+".jpg");
+            map.put("title", "标题"+i);
+            map.put("content", "内容"+i);
+            list.add(map);
+        }
+        return list;
+    }
+
+}
